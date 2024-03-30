a18a9b9bc2e127b11bf94c52b2df1e5c561be3aa
DJ Hersh <DanielJHersh@gmail.com>
2024-03-30 14:57:08 -0700

    configured gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/portfolio-nextjs",
+    output: "export",
+    reactStrictMode: true,
+    images: {
+        unoptimized: true
+    }
+};
 
 export default nextConfig;
