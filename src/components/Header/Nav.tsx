24249589756ddf1543912a8ed052253f755142f3
DJ Hersh <DanielJHersh@gmail.com>
2024-03-31 19:26:19 -0700

    added animations to header

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import Button from "../reusable/Button";
 
 type prop = {
   setShowNav: Function;
@@ -18,15 +19,15 @@ const Nav = ({ setShowNav }: prop) => {
   const NavLink = ({ page }: navProps) => {
     return (
       <div
-        className="px-4 py-2 text-center hover:bg-cyan-700 md:rounded hover:text-white uppercase hover:shadow-sm hover:shadow-black cursor-pointer"
+        // className="px-4 py-2 text-center hover:bg-cyan-700 md:rounded hover:text-white uppercase hover:shadow-sm hover:shadow-black cursor-pointer"
         onClick={() => scrollToSection(page)}
       >
-        <p>{page}</p>
+        <Button text={page} className="border-none w-full" />
       </div>
     );
   };
   return (
-    <div className="fixed md:flex md:top-3 md:pr-1 md:sticky md:bg-transparent top-[55.5px] bg-white bg-opacity-90 shadow-2xl md:shadow-none rounded-bl-lg right-0 text-cyan-700 text-2xl md:text-xl">
+    <div className="fixed text-center md:flex md:top-3 md:pr-1 md:sticky md:bg-transparent top-[55.5px] bg-white bg-opacity-90 shadow-2xl md:shadow-none rounded-bl-lg right-0 text-cyan-700 text-2xl md:text-xl">
       <NavLink page="About" />
       <NavLink page="Experience" />
       <NavLink page="Work" />
--- src/components/reusable/Button.tsx
+++ src/components/reusable/Button.tsx
@@ -4,11 +4,12 @@ type props = {
     text: string
     type?: any
     value?: string
+    className?: string
 }
 
-const Button = ({ text, type, value }: props) => {
+const Button = ({ text, type, value, className }: props) => {
   return (
-    <button className="button-swipe" type={type} value={value}>
+    <button className={`button-swipe ${className}`} type={type} value={value}>
         {text}
     </button>
   )
